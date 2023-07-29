8de113783881dcd5d19e09f82577671bb2e168ec
kaouther <kaoutherboutheldja@gmail.com>
2023-07-30 00:59:55 +0100

    Update sidebar

--- app.js
+++ app.js
@@ -3,33 +3,32 @@
 const menuIcon = document.querySelector('.menu');
 const closeIcon = document.querySelector('.close');
 const navLinks = document.querySelector('.nav-links');
+const sidebar = document.querySelector('.sidebar');
+const container = document.querySelector('.container');
+const overlay = document.querySelector('.overlay');
 
 // Function to show the navigation sidebar
 function showNavSidebar() {
-  navLinks.classList.add('show-sidebar');
+  sidebar.classList.add('show-sidebar');
+  container.classList.add('shade'); 
+  overlay.style.display = 'block'; 
+  closeIcon.style.display = 'block'; 
+  menuIcon.style.display = 'none'; 
 }
 
 // Function to hide the navigation sidebar
 function hideNavSidebar() {
-  navLinks.classList.remove('show-sidebar');
+  sidebar.classList.remove('show-sidebar');
+  container.classList.remove('shade'); 
+  overlay.style.display = 'none'; 
+  closeIcon.style.display = 'none'; 
+  menuIcon.style.display = 'block'; 
 }
 
-// Event listener for the menu icon click
 menuIcon.addEventListener('click', () => {
-  // Show the navigation sidebar
   showNavSidebar();
-  // Hide the menu icon
-  menuIcon.style.display = 'none';
-  // Show the close icon
-  closeIcon.style.display = 'block';
 });
 
-// Event listener for the close icon click
 closeIcon.addEventListener('click', () => {
-  // Hide the navigation sidebar
   hideNavSidebar();
-  // Hide the close icon
-  closeIcon.style.display = 'none';
-  // Show the menu icon
-  menuIcon.style.display = 'block';
 });
